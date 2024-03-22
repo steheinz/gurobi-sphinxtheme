00f67bccca8b3d6069273bbe3fb39bf62a7e7709
Simon Bowly <bowly@gurobi.com>
2024-03-23 00:20:28 +1100

    Store conf.py settings
    Many of furo's options don't seem to translate through theme.conf
    (in particular those needing nested dictionaries). This seems like
    a possible workaround: just have projects using this theme import
    their conf.py variables from our theme. This could also be used for
    non-html configuration (i.e. latex).

--- gurobi_sphinxtheme.py
+++ gurobi_sphinxtheme.py
@@ -1,4 +1,39 @@
 from os import path
 
+__all__ = [
+    "html_theme",
+    "html_theme_options",
+    "html_favicon",
+    "html_css_files",
+    "copyright",
+    "author",
+]
+
+copyright = '2024, Gurobi Optimization'
+
+author = 'Gurobi Optimization'
+
+html_theme = "gurobi_sphinxtheme"
+
+html_theme_options = {
+        "light_css_variables": {
+            "color-brand-primary": "#DD2113",
+            "color-brand-content": "#DD2113",
+            "font-size-normal": "50%",
+        },
+        "dark_css_variables": {
+            "color-brand-primary": "#DD2113",
+            "color-brand-content": "#DD2113",
+            "font-size-normal": "50%",
+        },
+        "sidebar_hide_name": True,
+        "light_logo": "gurobi_light.svg",
+        "dark_logo": "gurobi_dark.svg",
+    }
+
+html_favicon = "https://www.gurobi.com/favicon.ico"
+
+html_css_files = ['custom.css']
+
 def setup(app):
     app.add_html_theme("gurobi_sphinxtheme", path.abspath(path.dirname(__file__)))
