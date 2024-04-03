19eb5d7cca4278b9cf4e5137287c462859aec0a2
Matthias Miltenberger <miltenberger@gurobi.com>
2024-04-03 12:39:06 +0200

    Make links etc blue instead of red (#1)

--- gurobi_sphinxtheme/conf.py
+++ gurobi_sphinxtheme/conf.py
@@ -16,12 +16,12 @@ html_theme = "gurobi_sphinxtheme"
 html_theme_options = {
         "light_css_variables": {
             "color-brand-primary": "#DD2113",
-            "color-brand-content": "#DD2113",
+            "color-brand-content": "#1675a9",
             "font-size-normal": "50%",
         },
         "dark_css_variables": {
             "color-brand-primary": "#DD2113",
-            "color-brand-content": "#DD2113",
+            "color-brand-content": "#1675a9",
             "font-size-normal": "50%",
         },
         "sidebar_hide_name": True,
