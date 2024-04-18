dc40058757df39ec89c29d2f10b37bd93df7dbc8
Simon Bowly <bowly@gurobi.com>
2024-04-18 12:38:54 +1000

    Detect github issue url from RTD config

--- gurobi_sphinxtheme/__init__.py
+++ gurobi_sphinxtheme/__init__.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import re
 
 here = pathlib.Path(__file__).parent
 
@@ -7,16 +8,33 @@ here = pathlib.Path(__file__).parent
 def setup_context(app, pagename, templatename, context, doctree):
     if os.environ.get("READTHEDOCS") == "True":
 
+        # Version and url information. Store these in distinct jinja variables
+        # to prevent clashes with themes we inherit from.
         context["gurobi_rtd"] = "true"
         context["gurobi_rtd_version"] = os.environ.get("READTHEDOCS_VERSION")
         context["gurobi_rtd_version_type"] = os.environ.get("READTHEDOCS_VERSION_TYPE")
-        context["gurobi_rtd_canonical_url"] = os.environ.get("READTHEDOCS_CANONICAL_URL")
+        context["gurobi_rtd_canonical_url"] = os.environ.get(
+            "READTHEDOCS_CANONICAL_URL"
+        )
 
+        # For branch (i.e. not pull request) builds, get the canonical URL of
+        # the stable version.
         if context["gurobi_rtd_version_type"] == "branch":
-            stem, mid, _ = context["gurobi_rtd_canonical_url"].rpartition(context["gurobi_rtd_version"])
-            assert mid
-            assert stem.endswith("/")
+            stem, mid, _ = context["gurobi_rtd_canonical_url"].rpartition(
+                context["gurobi_rtd_version"]
+            )
+            assert mid and stem.endswith("/")
             context["gurobi_rtd_stable_url"] = stem + "stable/"
+
+        # URL for the issues page of the source repo.
+        git_clone_url = os.environ.get("READTHEDOCS_GIT_CLONE_URL")
+        match = re.match(r"git@github\.com:Gurobi/([\w-]+)\.git", git_clone_url)
+        if not match:
+            raise ValueError(f"Unexpected value: GIT_CLONE_URL={git_clone_url}")
+        repo_name = match.group(1)
+        context["gurobi_gh_issue_url"] = (
+            f"https://github.com/Gurobi/{repo_name}/issues/new?labels=bug&template=bug_report.md"
+        )
 
 
 def setup(app):
