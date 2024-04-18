a706cc4f8e9ce29a60e16ef5c4bbfeab6390d972
Simon Bowly <bowly@gurobi.com>
2024-04-18 11:29:15 +1000

    Read context in setup

--- gurobi_sphinxtheme/__init__.py
+++ gurobi_sphinxtheme/__init__.py
@@ -1,7 +1,24 @@
+import os
 import pathlib
 
 here = pathlib.Path(__file__).parent
 
 
+def setup_context(app, pagename, templatename, context, doctree):
+    if os.environ.get("READTHEDOCS") == "True":
+        context["gurobi_rtd"] = "true"
+        for env_var in [
+            "project",
+            "version",
+            "version_name",
+            "version_type",
+            "canonical_url",
+        ]:
+            context[f"gurobi_rtd_{env_var}"] = os.environ.get(
+                f"READTHEDOCS_{env_var.upper()}", ""
+            )
+
+
 def setup(app):
     app.add_html_theme("gurobi_sphinxtheme", here / "theme")
+    app.connect("html-page-context", setup_context)
