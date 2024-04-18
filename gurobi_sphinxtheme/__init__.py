1958d9ea0efde1c70fd5efb4db0f766c07a525a9
Simon Bowly <bowly@gurobi.com>
2024-04-18 11:59:49 +1000

    Draft dev/old warning banners

--- gurobi_sphinxtheme/__init__.py
+++ gurobi_sphinxtheme/__init__.py
@@ -6,17 +6,17 @@ here = pathlib.Path(__file__).parent
 
 def setup_context(app, pagename, templatename, context, doctree):
     if os.environ.get("READTHEDOCS") == "True":
+
         context["gurobi_rtd"] = "true"
-        for env_var in [
-            "project",
-            "version",
-            "version_name",
-            "version_type",
-            "canonical_url",
-        ]:
-            context[f"gurobi_rtd_{env_var}"] = os.environ.get(
-                f"READTHEDOCS_{env_var.upper()}", ""
-            )
+        context["gurobi_rtd_version"] = os.environ.get("READTHEDOCS_VERSION")
+        context["gurobi_rtd_version_type"] = os.environ.get("READTHEDOCS_VERSION_TYPE")
+        context["gurobi_rtd_canonical_url"] = os.environ.get("READTHEDOCS_CANONICAL_URL")
+
+        if context["gurobi_rtd_version_type"] == "branch":
+            stem, mid, _ = context["gurobi_rtd_canonical_url"].rpartition(context["gurobi_rtd_version"])
+            assert mid
+            assert stem.endswith("/")
+            context["gurobi_rtd_stable_url"] = stem + "stable/"
 
 
 def setup(app):
